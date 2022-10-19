fccfd0a255fe52a3342759487fab62917b0c9d60
triscillanandy <110343215+triscillanandy@users.noreply.github.com>
2022-10-19 11:35:35 +0300

    first commit

---
+++ database/firebase.js
@@ -0,0 +1,14 @@
+// database/firebaseDb.js
+// import { initializeApp } from 'firebase/app';
+// const firebaseConfig = {
+//    apiKey: "AIzaSyBYdiP3wfewLmONP_ZqZ0N6ISEib57sDi0",
+//    authDomain: "agromap-6f1b4.firebaseapp.com",
+//    databaseURL: "https://agromap-6f1b4-default-rtdb.firebaseio.com",
+//    projectId: "agromap-6f1b4",
+//    storageBucket: "agromap-6f1b4.appspot.com",
+//    messagingSenderId: "742095645955",
+//    appId: "1:742095645955:web:1d96c4d5d92dce0ab54d36",
+//    measurementId: "G-79EX5M8NYP"
+// };
+//firebase.initializeApp(firebaseConfig);
+//export default firebase;
