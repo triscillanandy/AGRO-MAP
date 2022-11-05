65c58ab89d3bca45667002a71975da67b6a1bfe9
triscillanandy <triscillanandy@gmail.com>
2022-11-05 10:25:49 +0300

    index

--- src/screens/index.js
+++ src/screens/index.js
@@ -2,3 +2,10 @@ export {default as Splash} from './Splash';
 export {default as Login} from './Login';
 export {default as Signup} from './Signup';
 export {default as Dashboard} from './Dashboard';
+export {default as Passreset} from './Passreset';
+export {default as Home} from './Home';
+export {default as Map} from './Map';
+export {default as Chat} from './Chat';
+export {default as Lib} from './Lib';
+
+export {default as Profile} from './Profile'
