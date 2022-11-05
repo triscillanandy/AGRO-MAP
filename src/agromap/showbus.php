f2c82850864aea44437b713ee3328801bd58184f
triscillanandy <triscillanandy@gmail.com>
2022-11-05 10:09:02 +0300

    Create showbus.php
    fetch and display data from database

---
+++ src/agromap/showbus.php
@@ -0,0 +1,30 @@
+
+<?php
+include('db.php');
+ 
+
+
+ 
+// Creating SQL command to fetch all records from Table.
+$sql = "SELECT * FROM  business";
+
+ 
+$result = $conn->query($sql);
+ 
+if ($result->num_rows >0) {
+ 
+ 
+ while($row[] = $result->fetch_assoc()) {
+ 
+ $item = $row;
+ 
+ $json = json_encode($item);
+ 
+ }
+ 
+} else {
+ echo "No Results Found.";
+}
+ echo $json;
+$conn->close();
+?>
