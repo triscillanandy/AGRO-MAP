e3c801c6bdf2a50ff1799c62efe2b7fbe67c0c78
triscillanandy <triscillanandy@gmail.com>
2022-11-05 10:28:00 +0300

    splash screen added

--- src/screens/Splash.js
+++ src/screens/Splash.js
@@ -8,20 +8,12 @@ const Splash = ({ navigation }) => {
     setTimeout(() => {
         navigation.replace('Login')
     }, 3000)
-    return ( <View style = {
-            { flex: 1, flexDirection: 'column', justifyContent: 'center', alignItems: 'center', backgroundColor:"#08E4C4"}
-        } >
-        <StatusBar barStyle = "light-content"
-        hidden = { false }
-        backgroundColor = "#465bd8"/>
-        <Image source = { require('../assets/images/bfam.png') }
-        style = {
-            { width: 50, height: 50 }
-        }
-        />   
-          <Text style = {
-            {fontSize: 30} }>Nuntium</Text> 
-            </View>
+    return (
+        <View style = {{ flex: 1, flexDirection: 'column', justifyContent: 'center', alignItems: 'center', backgroundColor:"#08E4C4"}} >
+            <StatusBar barStyle = "light-content" hidden = { false } backgroundColor = "#465bd8"/>
+            <Image source = { require('../assets/images/bfam.png') } style = {{  width:220.91 ,height:200, marginLeft:10, marginTop:10 }}/>   
+            <Text style = {{fontSize: 30} }>AGRO MAP</Text> 
+        </View>
     )
 }
 
