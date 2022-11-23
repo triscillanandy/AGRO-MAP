d98bf5e34e5720a33e6c14413bfacbb986beae68
triscillanandy <triscillanandy@gmail.com>
2022-11-23 17:17:44 +0300

    two places mapped

--- src/screens/Map.js
+++ src/screens/Map.js
@@ -1,24 +1,80 @@
-import * as React from 'react';
-import MapView from 'react-native-maps';
-import { StyleSheet, Text, View, Dimensions } from 'react-native';
 
-export default function App() {
-  return (
-    <View style={styles.container}>
-      <MapView style={styles.map} />
-    </View>
-  );
-}
+import { Dimensions, StyleSheet } from 'react-native';
+import MapView, { Marker } from 'react-native-maps';
+import MapViewDirections from 'react-native-maps-directions';
+import React, { useState,useRef } from 'react';
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+//const { width, height } = Dimensions.get('window');
+
+
+
+const GOOGLE_MAPS_APIKEY = 'AIzaSyAINHQ2ZTUf3comgnPaABibAJCaBLFHQ2g';
+
+const Map = ()=> {
+  const [state,setstate] = useState({
+    pickupCords:{
+      latitude: 30.7046,
+      longitude: 76.7179,
+      latitudeDelta: 0.0922,
+      longitudeDelta: 0.0421,
+
   },
-  map: {
-    width: Dimensions.get('window').width,
-    height: Dimensions.get('window').height,
-  },
-});
+  droplocationCords:{
+    latitude: 30.7333,
+    longitude: 76.7794,
+    latitudeDelta: 0.0922,
+    longitudeDelta: 0.0421,
+
+  }
+})
+const mapRef = useRef()
+
+const { pickupCords,droplocationCords} =state
+  
+
+ 
+    return (
+   
+      <MapView
+      ref={mapRef}
+      initialRegion={pickupCords}
+      style={StyleSheet.absoluteFill}
+        
+      >
+        <Marker coordinate={pickupCords}
+        />
+        <Marker coordinate={droplocationCords}
+        />
+        
+       
+          <MapViewDirections
+          origin={pickupCords}
+         
+          destination={droplocationCords}
+          apikey={GOOGLE_MAPS_APIKEY}
+          strokeWidth={3}
+          strokeColor="hotpink"
+          optimizeWaypoints={true}
+          onReady={result => {
+            mapRef.current.fitToCoordinates(result.coordinates,{
+              edgePadding:{
+                right:30,
+                bottom:300,
+                left:30,
+                top:100
+              }
+            })
+          }}
+
+         
+         
+          
+        />
+     
+    </MapView>
+       );
+      }
+    
+    
+    export default Map;
+  
